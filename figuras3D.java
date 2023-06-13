d9a6042cf1b070568baf40cc31111a799c3614c1
Melany Barrera <Melani_nayhely@outlook.es>
2023-06-12 20:44:52 -0500

    Update Methods, Atributtes and Elements

--- figuras3D.java
+++ figuras3D.java
@@ -7,7 +7,9 @@ public class figuras3D {
     private double base_prisma; //Atributo necesario para el prisma
     private double altura_prisma; //Atributo necesario para el prisma
     private double profundidad_prisma; //Atributo necesario para el prisma
-    private float altura_piramide; //Atributos para piramide
+
+    //Atributos para piramide
+    private float altura_piramide;
     private float base_piramide;
     private float apotema_piramide;
 
@@ -20,41 +22,26 @@ public class figuras3D {
     private double radio;
 
     //Constructores-------------------------------------------------------
-    //Bryan Delgado ---- Constructor de Icosaedro
-    public figuras3D(float a) {
+    //Constructor general
+
+    public figuras3D(float a, double pi, float radio_base_cono, float altura_cono, double base_prisma, double altura_prisma, double profundidad_prisma,
+                     float altura_piramide, float base_piramide, float apotema_piramide, int altura_cilindro, int base_cilindro, int radio_cilindro,
+                     double radio, float l) {
         this.a = a;
-    }
-    //Heyer Tinoco ---- Constructores Cono
-    public figuras3D(float radio_base_cono, float altura_cono) {
+        this.pi = pi;
         this.radio_base_cono = radio_base_cono;
         this.altura_cono = altura_cono;
-    }
-    //Martin Jimenez --- Constructores prisma
-    // Constructor con argumentos
-    public figuras3D(double base_prisma, double altura_prisma, double profundidad_prisma) {
         this.base_prisma = base_prisma;
         this.altura_prisma = altura_prisma;
         this.profundidad_prisma = profundidad_prisma;
-    }
-
-    //Erick Villarroel
-    public figuras3D(float altura_piramide, float apotema_piramide, float base_piramide) {
         this.altura_piramide = altura_piramide;
         this.base_piramide = base_piramide;
         this.apotema_piramide = apotema_piramide;
-    }
-
-    //Melany Barrera------------------------------------------------------
-    public figuras3D(int altura_cilindro, int base_cilindro, int radio_cilindro) {
         this.altura_cilindro = altura_cilindro;
         this.base_cilindro = base_cilindro;
         this.radio_cilindro = radio_cilindro;
-    }
-
-    //Ingrith Revelo------------------------------------------------------
-    public figuras3D(double pi, double radio) {
-        this.pi = pi;
         this.radio = radio;
+        this.l = l;
     }
 
     //Metodos-------------------------------------------------------------
@@ -92,17 +79,16 @@ public class figuras3D {
         return (float) (pi*aux);
     }
     private float area_lateral_cono(){ //Calculo necesario para calcular el area del cono
-        double aux = getRadio_base_cono()*getRadio_base_cono();
-        double aux2= getAltura_cono()*getAltura_cono();
+        double aux = Math.pow(radio_base_cono, 2);
+        double aux2= Math.pow(altura_cono, 2);
         double s = Math.sqrt(aux+aux2);
-        return (float) (pi*getRadio_base_cono()*s);
+        return (float) (pi*radio_base_cono*s);
     }
     private float area_total_cono(){ //Area TOTAL del cono
         return (area_base_cono()+area_lateral_cono());
     }
     private float volumen_cono(){ //Volumen Cono
-        double aux = getRadio_base_cono()*getRadio_base_cono();
-        return (float) ((1/3)*pi*aux*getAltura_cono());
+        return (float) ((1/3)*pi*Math.pow(radio_base_cono, 2)*altura_cono);
     }
     public void imprimir_datos_cono(){
         System.out.println("\nEl área total de un cono es: " +area_total_cono());
