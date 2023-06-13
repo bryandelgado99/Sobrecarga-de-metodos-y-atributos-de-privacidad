b0d34fcc4286a1f21245ba12578158a85b683904
Melany Barrera <Melani_nayhely@outlook.es>
2023-06-12 19:26:46 -0500

    Actualizacion y agregado de metodos - Esfera

--- figuras3D.java
+++ figuras3D.java
@@ -16,6 +16,9 @@ public class figuras3D {
     private int base_cilindro;
     private int radio_cilindro;
 
+    //Atributos para esfera
+    private double radio;
+
     //Constructores-------------------------------------------------------
     //Bryan Delgado ---- Constructor de Icosaedro
     public figuras3D(float a) {
@@ -46,6 +49,12 @@ public class figuras3D {
         this.altura_cilindro = altura_cilindro;
         this.base_cilindro = base_cilindro;
         this.radio_cilindro = radio_cilindro;
+    }
+
+    //Ingrith Revelo------------------------------------------------------
+    public figuras3D(double pi, double radio) {
+        this.pi = pi;
+        this.radio = radio;
     }
 
     //Metodos-------------------------------------------------------------
@@ -245,4 +254,25 @@ public class figuras3D {
     }
 
     //------------------------------------------------------------------
+
+    //Ingrith Revelo-----------------------------------------------------
+    public double getRadio() {return radio;}
+
+    public void setRadio(double radio) {this.radio = radio;}
+
+    public double getPi() {return pi;}
+
+    public void setPi(double pi) {this.pi = pi;}
+
+    private float area_esfera(){
+        return (float) (4*pi*Math.pow(radio, 2));
+    }
+    private float volumen_esfera(){
+        double op = (0.75*pi*Math.pow(radio,3));
+        return (float) op;
+    }
+    public void imprimir_datos_esfera(){
+        System.out.println("El area de la esfera es: "+area_esfera());
+        System.out.println("El volumen de la esfera es: "+volumen_esfera());
+    }
 }
