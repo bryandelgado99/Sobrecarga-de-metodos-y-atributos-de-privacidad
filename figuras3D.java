d0589d3bdea10a9bd7621c5ce7d4f6b4951cd0d8
Melany Barrera <Melani_nayhely@outlook.es>
2023-06-12 19:32:35 -0500

    Agregado de metodos, atributos y definicios de Esfera

--- figuras3D.java
+++ figuras3D.java
@@ -257,19 +257,14 @@ public class figuras3D {
 
     //Ingrith Revelo-----------------------------------------------------
     public double getRadio() {return radio;}
-
     public void setRadio(double radio) {this.radio = radio;}
-
     public double getPi() {return pi;}
-
     public void setPi(double pi) {this.pi = pi;}
-
     private float area_esfera(){
         return (float) (4*pi*Math.pow(radio, 2));
     }
     private float volumen_esfera(){
-        double op = (0.75*pi*Math.pow(radio,3));
-        return (float) op;
+        return (float) (0.75*pi*Math.pow(radio,3));
     }
     public void imprimir_datos_esfera(){
         System.out.println("El area de la esfera es: "+area_esfera());
